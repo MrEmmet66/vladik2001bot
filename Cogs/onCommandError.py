c7bdb811cc1953ce64f8cf9901805d7811061e26
MrDMen 228 <lorrenc7@gmail.com>
2021-08-21 18:18:35 +0300

    Add unknown command message

--- Cogs/onCommandError.py
+++ Cogs/onCommandError.py
@@ -1,8 +1,14 @@
 import discord
+import time
+import json
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions, CheckFailure, CommandNotFound, NotOwner
-import time
 
+with open("configuration.json", "r") as config: 
+	data = json.load(config)
+	token = data["token"]
+	prefix = data["prefix"]
+	owner_id = data["owner_id"]
 
 class OnCommandErrorCog(commands.Cog, name="on command error"):
 	def __init__(self, bot:commands.Bot):
@@ -23,7 +29,8 @@ class OnCommandErrorCog(commands.Cog, name="on command error"):
 			else:
 				await ctx.send(f'This command has a cooldown, for {error.retry_after:.2f} second(s)')
 		elif isinstance(error, CommandNotFound):
-			return
+			errorEmbed = discord.Embed(title="Ошибка", description="Команда не найдена, убедитесь, что вы ввели всё правильно.\nДля вывода списка команд введите {}help".format(prefix),  color=0xf70002)
+			await ctx.send(embed=errorEmbed)
 		elif isinstance(error, MissingPermissions):
  			await ctx.send(error)
 		elif isinstance(error, CheckFailure):
