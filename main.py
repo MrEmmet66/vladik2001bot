ded8fa651989cb138c1b78ad94b8741762a01e90
MrDMen 228 <lorrenc7@gmail.com>
2021-08-21 18:19:41 +0300

    Add command arg requirement

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from inspect import ArgInfo
 import discord
-from discord import embeds
-from discord.errors import NotFound
-from discord.ext import commands
 import json
 import os
 import pyowm
+from discord import embeds
+from discord.ext import commands
+from discord.ext.commands import MissingPermissions, CheckFailure, CommandNotFound, NotOwner
 from pyowm.utils.config import get_default_config
 from datetime import date, datetime
+from inspect import ArgInfo
 
 # Get configuration.json
 with open("configuration.json", "r") as config: 
@@ -47,11 +47,12 @@ async def on_ready():
 		embed.add_field(name="{}help".format(prefix), value="Выводит данное сообщение", inline=False)
 		embed.add_field(name="{}ping".format(prefix), value="Выводит задержку между ботом и сервером", inline=False)
 		embed.add_field(name="{}weather [город]".format(prefix), value="Показывает погоду в указанном вами городе", inline=False)
+		embed.add_field(name="{}avatar [@user]".format(prefix), value="Показывает аватар указанного пользователя", inline=False)
 		embed.set_author(name="Vladik2001", icon_url=bot.user.avatar_url)
 		await ctx.send(embed=embed)
 
 @bot.command(pass_context = True)
-async def calculate(ctx, *, nums):
+async def calculate(ctx, *, nums: str):
 	math_expression = nums
 	if 'os' not in math_expression:
 		try:
@@ -62,7 +63,7 @@ async def calculate(ctx, *, nums):
 			await ctx.send("Результат: %s = %s" % (math_expression, result))
 
 @bot.command(pass_context = True)
-async def weather(ctx, city):
+async def weather(ctx, city: str):
 	place = city
 	config_dict = get_default_config()
 	config_dict['language'] = 'ru'
@@ -89,4 +90,20 @@ async def weather(ctx, city):
 	embed.set_footer(text=f"Текущее время: {dt_string}")
 	await ctx.send(embed=embed)
 
+@weather.error
+async def weather_error(ctx, error):
+	if isinstance(error, commands.MissingRequiredArgument):
+		errorEmbed = discord.Embed(title="Ошибка", color=0xf70002)
+		errorEmbed.add_field(name="Пожалуйста, укажите город", value="Пример использования: {}weather Москва".format(prefix))
+		await ctx.send(embed=errorEmbed)
+
+
+@calculate.error
+async def calculate_error(ctx, error):
+	if isinstance(error, commands.MissingRequiredArgument):
+		errorEmbed = discord.Embed(title="Ошибка", color=0xf70002)
+		errorEmbed.add_field(name="Пожалуйста, укажите аргументы вычисления", value="Пример использования: {}calculate 2 + 2".format(prefix))
+		await ctx.send(embed=errorEmbed)
+
 bot.run(token)
+	
