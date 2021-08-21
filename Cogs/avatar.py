402b87d3ef59ab3cde564a116be0dc57d1aa0a86
MrDMen 228 <lorrenc7@gmail.com>
2021-08-21 15:51:38 +0300

    Added avatar command

---
+++ Cogs/avatar.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import time
+
+
+class AvatarCog(commands.Cog, name="get avatar command"):
+	def __init__(self, bot:commands.bot):
+		self.bot = bot
+        
+	@commands.command(name = "avatar", description = "Get user avatar")
+	async def avatar(self, ctx, member: discord.Member = None):
+            if member is None:
+                member = ctx.author
+            avatarEmbed = discord.Embed(title=f"Avatar of user {member.name}", timestamp=ctx.message.created_at, color=0x3ee08c)
+            avatarEmbed.set_image(url=member.avatar_url)
+            await ctx.send(embed = avatarEmbed)
+
+def setup(bot:commands.Bot):
+	bot.add_cog(AvatarCog(bot))
